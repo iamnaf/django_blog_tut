16fff0d5589db627cd4960104b92e6c0b7b7924a
iamnaf <nafsulaiman@gmail.com>
2021-08-29 03:33:41 +0100

    add post editing page

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateNew, BlogUpdateView
 
 urlpatterns = [
+    path('post/<int:pk>/edit', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/new/', BlogCreateNew.as_view(), name='post_new'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('', BlogListView.as_view(), name='home'),
 ]
