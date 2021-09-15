407b90d6313662ea5f6dc7e37df6da16ccae5a08
iamnaf <nafsulaiman@gmail.com>
2021-09-15 17:03:16 +0100

    run python manage.py collectstatic

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oq)pw*om!z(c$(2_-l8sgy(4g0pcmttneniokif663g5!pwxr4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
